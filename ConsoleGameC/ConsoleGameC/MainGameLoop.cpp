2dd451e23faa417551b74189a05a469c501ae971
WaddleGee <Georgeshapka@gmail.com>
2025-11-21 21:10:09 -0500

    changed position from up and right to down and right. made the colision tracker actually work. added collision detection for up and down.

--- ConsoleGameC/ConsoleGameC/MainGameLoop.cpp
+++ ConsoleGameC/ConsoleGameC/MainGameLoop.cpp
@@ -5,9 +5,9 @@
 //map measurements
 #define MAP_SIZE 1500
 #define MAP_HEIGHT 16
-#define MAP_WIDTH 35
+#define MAP_WIDTH_PLUS1 33//add extra character for \0
 #define MAP1_FILE "../../map1.txt"
-#define SPACES_TO_REACH_TOP 16
+#define SPACES_TO_REACH_BOTTOM 16
 #define SPACES_TO_REACH_RIGHT 32
 
 //sleep amounts
@@ -24,14 +24,19 @@
 //cursor strings
 #define CURSOR_UNDER_MAP "\033[H\033[17B"
 #define CURSOR_HOME "\033[H"
+#define CURSOR_HIDE "\033[?25l"
+#define CURSOR_SHOW "\033[?25h"
+#define CLEAR_SCREEN "\033[2J"
 
 //character codes
-#define WALL_CHARACTER_LEFTRIGHT 186
+#define WALL_CHARACTER_VERTICAL 186
 
 
 
 int main(void)
 {
+	printf(CURSOR_HIDE);
+
 	FILE* mapFile;
 	fopen_s(&mapFile, MAP1_FILE, "r");
 	if (mapFile == NULL)
@@ -40,25 +45,37 @@ int main(void)
 		return EXIT_FAILURE;
 	}
 
-	char mapString[MAP_SIZE] = { 0 };
+	unsigned char mapString[MAP_SIZE] = { 0 };
+	unsigned char tempMapString[MAP_SIZE] = { 0 };
 	fread_s(mapString, MAP_SIZE, 1, MAP_SIZE, mapFile);
+	strncpy_s((char*)tempMapString, MAP_SIZE, (char*)mapString, MAP_SIZE);
 
 	//use this to track where collision objects are
-	unsigned char mapTracker[MAP_HEIGHT][MAP_WIDTH] = { { 0 } };
+	unsigned char mapTracker[MAP_HEIGHT][MAP_WIDTH_PLUS1] = { { 0 } };
+
+	//copy map to collision tracker
+	char* nextLine = NULL;
+	char* line = strtok_s((char*)tempMapString, "\n", &nextLine);
 	for (int i = 0; i < MAP_HEIGHT; i++)
 	{
-		sscanf_s(mapString, "%[^\n]", mapTracker[i], MAP_WIDTH);
+		strncpy_s((char*)mapTracker[i], MAP_WIDTH_PLUS1, line, MAP_WIDTH_PLUS1);
+		mapTracker[i][MAP_WIDTH_PLUS1 - 1] = '\0';
+		line = strtok_s(NULL, "\n", &nextLine);
 	}
-	//printf("%s\n", mapString);
 
-	int u = 8;
-	int d = 0;//unused
-	int r = 16;
+
+	//starting position
+	int u = 0;//unused
+	int d = 8;
+	int r = 10;
 	int l = 0;//unused
 
 	bool doLoop = true;
-	int sleepSprint = 200;
-	bool skipSleep = false;
+	int sleepSprint = SPRINT_SLEEP;
+	//by default skip sprint to make key presses more responsive
+	//only do the sprint if player is moving
+	bool skipSprint = true;
+
 	while (doLoop)
 	{
 		//print the map
@@ -66,53 +83,58 @@ int main(void)
 		printf("\n%s", mapString);
 
 		//move to current position and print player
-		printf(CURSOR_UNDER_MAP);
+		printf(CURSOR_HOME);
 		printf("\033[%dA\033[%dB\033[%dC\033[%dD", u, d, r, l);
 		printf("%c", 254);
 
 		//get input and set position
-		//up/down is 1 indexed
-		//left/right is 0 indexed
+		//up/down and left/right are 1 indexed
 		
 		//up
 		if (GetAsyncKeyState(W_KEY) & 0x8000)
 		{
-			if (u <= SPACES_TO_REACH_TOP)
+			if (mapTracker[d - 2][r - 1] != WALL_CHARACTER_VERTICAL)
 			{
-				u++;
-				skipSleep = false;
+				if (d > 1)
+				{
+					d--;
+					skipSprint = false;
+				}
 			}
 		}
 		//down
 		if (GetAsyncKeyState(S_KEY) & 0x8000)
 		{
-			if (u > 1)
+			if (mapTracker[d][r - 1] != WALL_CHARACTER_VERTICAL)
 			{
-				u--;
-				skipSleep = false;
+				if (d < SPACES_TO_REACH_BOTTOM)
+				{
+					d++;
+					skipSprint = false;
+				}
 			}
 		}
 		//left
 		if (GetAsyncKeyState(A_KEY) & 0x8000)
 		{
-			if (mapTracker[u][r - 2] != WALL_CHARACTER_LEFTRIGHT)
+			if (mapTracker[d - 1][r - 2] != WALL_CHARACTER_VERTICAL)
 			{
 				if (r > 1)
 				{
 					r--;
-					skipSleep = false;
+					skipSprint = false;
 				}
 			}
 		}
 		//right
 		if (GetAsyncKeyState(D_KEY) & 0x8000)
 		{
-			if (mapTracker[u][r] != WALL_CHARACTER_LEFTRIGHT)
+			if (mapTracker[d - 1][r] != WALL_CHARACTER_VERTICAL)
 			{
 				if (r <= SPACES_TO_REACH_RIGHT)
 				{
 					r++;
-					skipSleep = false;
+					skipSprint = false;
 				}
 			}
 		}
@@ -120,7 +142,7 @@ int main(void)
 		if (GetAsyncKeyState(VK_SHIFT) & 0x8000)
 		{
 			sleepSprint = SPRINT_SLEEP;
-			skipSleep = false;
+			skipSprint = false;
 		}
 		else
 		{
@@ -133,10 +155,14 @@ int main(void)
 		}
 
 		//debug
-		printf(CURSOR_UNDER_MAP);
-		printf("u=%d r=%d",u, d);
+		{
+			printf(CURSOR_UNDER_MAP);
+			//extra space to clear old values if 1 digit
+			printf("d=%d r=%d       ",d, r);
+		}
+		//end of debug
 
-		if (!skipSleep)
+		if (!skipSprint)
 		{
 			Sleep(sleepSprint);
 		}
@@ -144,6 +170,8 @@ int main(void)
 		{
 			Sleep(8);
 		}
-		skipSleep = true;
+		skipSprint = true;
 	}//end of while loop
+
+	printf(CLEAR_SCREEN CURSOR_HOME);
 }
